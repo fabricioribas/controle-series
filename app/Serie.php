bd6e69457041304ecbe92848d94bd504429a9356
“Fabrício Ribas“ <fabricioaribas@gmail.com>
2020-03-09 20:58:22 -0300

    Criação das Temporadas e Episodios

--- app/Serie.php
+++ app/Serie.php
@@ -11,4 +11,10 @@ class Serie extends Model
     // Toda propriedade que quisermos passar pelo CREATE (Controller) é necessário informar dentro do Atributo fillable(preenchivél).
     // Assim informamos quais Atributos permitimos serem enviados dessa forma.
     protected $fillable = ['nome'];
+
+    public function temporadas()
+    {
+        // Existe uma relação em que uma Série($this), tem muitas (hasMany), Temporadas(Temporadas::class)
+        return $this->hasMany(Temporada::class);
+    }
 }
