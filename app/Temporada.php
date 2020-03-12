704d3516f5cd694b1ce0feac6c09f354ab8a83bc
“Fabrício Ribas“ <fabricioaribas@gmail.com>
2020-03-12 19:23:45 -0300

    Selecionar episodios visualizados

--- app/Temporada.php
+++ app/Temporada.php
@@ -1,9 +1,10 @@
 <?php
-
+// -----------------------------------------------------------------------------
 namespace App;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-
+// -----------------------------------------------------------------------------
 class Temporada extends Model
 {
     public $timestamps = false;
@@ -17,5 +18,12 @@ class Temporada extends Model
     public function serie()
     {
         return $this->belongsTo(Serie::class);
+    }
+
+    public function getEpisodiosAssistidos(): Collection
+    {
+        return $this->episodios->filter(function (Episodio $episodio) {
+            return $episodio->assistido;
+        });
     }
 }
